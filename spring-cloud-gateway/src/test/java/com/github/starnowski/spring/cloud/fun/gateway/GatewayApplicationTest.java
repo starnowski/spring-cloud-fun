e9389d35e0f82b7f8e0229b38b5bbdc03216b51f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-10-14 14:03:03 +0200

    Added application tests

---
+++ spring-cloud-gateway/src/test/java/com/github/starnowski/spring/cloud/fun/gateway/GatewayApplicationTest.java
@@ -0,0 +1,14 @@
+package com.github.starnowski.spring.cloud.fun.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GatewayApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
