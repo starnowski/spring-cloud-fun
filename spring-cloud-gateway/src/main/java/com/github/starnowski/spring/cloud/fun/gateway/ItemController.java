20846d5732c7902f80b0124d9e3e8ec77e3e7277
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-04-16 20:02:57 +0200

    Added tests case

--- spring-cloud-gateway/src/main/java/com/github/starnowski/spring/cloud/fun/gateway/ItemController.java
+++ spring-cloud-gateway/src/main/java/com/github/starnowski/spring/cloud/fun/gateway/ItemController.java
@@ -2,27 +2,41 @@ package com.github.starnowski.spring.cloud.fun.gateway;
 
 import com.github.starnowski.spring.cloud.fun.gateway.model.Item;
 import com.github.starnowski.spring.cloud.fun.gateway.services.ItemService;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @Validated
 public class ItemController {
 
-    private final ItemService itemService;
+    @Autowired
+    private ItemService itemService;
 
     public ItemController(@Autowired ItemService itemService) {
         this.itemService = itemService;
     }
 
     @GetMapping("/item/{id}")
-    public ResponseEntity<Item> get(@PathVariable("id") @Size(max = 15) String id) {
+    public ResponseEntity<Item> get(@Valid @PathVariable("id") @Size(max = 15) String id) {
         return ResponseEntity.ok(itemService.getItemById(id));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleValidationExceptions(ConstraintViolationException ex) {
+        StringBuilder errorMessages = new StringBuilder();
+        ex.getConstraintViolations().forEach(error -> {
+            errorMessages.append(error.getMessage()).append(". ");
+        });
+        return ResponseEntity.badRequest().body(errorMessages.toString());
     }
 }
--- spring-cloud-gateway/src/test/groovy/com/github/starnowski/spring/cloud/fun/gateway/ItemControllerTest.groovy
+++ spring-cloud-gateway/src/test/groovy/com/github/starnowski/spring/cloud/fun/gateway/ItemControllerTest.groovy
@@ -1,35 +1,56 @@
 package com.github.starnowski.spring.cloud.fun.gateway
 
+import com.github.starnowski.spring.cloud.fun.gateway.model.Item
 import com.github.starnowski.spring.cloud.fun.gateway.services.ItemService
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.RequestBuilder
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@WebMvcTest(ItemController)
 class ItemControllerTest extends Specification {
 
+    @Autowired
     MockMvc mockMvc
 
-    ItemController itemController
-
-    ItemService itemService
-
-    def setup(){
-        itemService = Mock(ItemService)
-        itemController = new ItemController(itemService)
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(itemController)
-                .build();
-    }
+    @SpringBean
+    ItemService itemService = Mock(ItemService)
 
     def "should return bad request when path variable size is outbound"(){
         when:
-            def result = mockMvc.perform(MockMvcRequestBuilders.get("/item/{id}", "1234123412341234")).andDo(MockMvcResultHandlers.print())
+            def result = mockMvc.perform(MockMvcRequestBuilders.get("/item/{id}", "12341234123412341234123412341234"))
+                    .andDo(MockMvcResultHandlers.print())
 
         then:
             result.andExpect(MockMvcResultMatchers.status().isBadRequest())
+    }
+
+    @Unroll
+    def "should return expected item #item when id is #id"(){
+        given:
+            itemService.getItemById(id) >> item
+
+        when:
+            def result = mockMvc.perform(MockMvcRequestBuilders.get("/item/{id}", id))
+                .andDo(MockMvcResultHandlers.print())
+
+        then:
+            result.andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().json("{\"id\": \"dfasdfadf\", \"name\": \"deser\"}"))
+
+        where:
+            id                  |    item
+            "dfasdfadf"         |   new Item("dfasdfadf", "deser")
     }
 }
